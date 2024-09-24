0c7f83bf58c71cd07dad290a1d75e859d29a8f63
Abel Nigus <abinig5@gmail.com>
2024-09-24 17:08:24 +0300

    add next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "5000",
         pathname: "/upload-local/public/**",
       },
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dcux0rynq/image/upload/**",
+      },
     ],
   },
 };
